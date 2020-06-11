cb5c991c280355015fbc6d17463247d57932196a
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-11 15:44:01 -0300

    Sinraxe alternativa com laco foreach

---
+++ LacoForeach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LacoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Bob", "Alex", "Paula", "Adriano" };
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
